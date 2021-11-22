0bc257b379256293537e6321fceb4d09011146bc
Christophoros-Spyretos <cspyretos@gmail.com>
2021-11-22 11:48:59 +0100

    Assignment 3 fix

--- Labs/Lab01/Assignment 3.R
+++ Labs/Lab01/Assignment 3.R
@@ -4,27 +4,35 @@
 
 data <- read.csv("Labs/Lab01/pima-indians-diabetes.csv", header = FALSE)
 
-colnames(data) <- c("TimesPregnant", "Plasma", "Diastolic", "Triceps",
-                    "Insulin", "MassIndex", "Pedigree", "Age", "Diabetes")
+colnames(data) <- c("Pregnancies",
+                  "Glucose",
+                  "BloodPressure",
+                  "SkinThickness",
+                  "Insulin",
+                  "BMI",
+                  "DiabetesPedigreeFunction",
+                  "Age",
+                  "Diabetes")
+
+data$Diabetes <- as.factor(data$Diabetes)
+levels(data$Diabetes) <- c("No","Yes")
 
 # Task 1 
 
 library(ggplot2)
 
-scatterplot <- ggplot( data = data, aes( x = Age, y = Plasma, colour = as.factor(Diabetes))) +
-  geom_point() + scale_color_manual(name = "Diabetes", 
-                                    labels = c("Non-Diabetic ", "Diabetic "),
-                                    values =c("#F8766D", "#00BFC4"))
+scatterplot <- ggplot(data, aes(x = Age, y = Glucose, col = Diabetes)) +
+  geom_point() +
+  theme_classic() +
+  ggtitle("Glucose vs. Age coloured by Diabetes Level")
 
 scatterplot
 
 # Task 2
+library(caret)
 
-fit <- glm( Diabetes ~ Age + Plasma, data = data, family = binomial)
-probabilities <- predict(object = fit, newdata =  data, type = "link")
-predicted_values <- ifelse(probabilities > 0.5, "pos", "neg")
-
-data$Predict <- predicted_values
+glm_fit <- train(Diabetes ~ Age + Glucose, data = data, method="glm")
+data$PredDiabetes <- predict(glm_fit) # predict function uses 0.5 as default value
 
 misclass <- function(actual_val,predicted_val){
   
@@ -36,20 +44,20 @@ misclass <- function(actual_val,predicted_val){
   return(error)
 }
 
-misclass(data$Diabetes, data$Predict)
+misclass(data$Diabetes,data$PredDiabetes)
 
-scatterplot2 <- ggplot( data = data, aes( x = Age, y = Plasma, colour = Predict)) +
-  geom_point() + scale_color_manual(name = "Diabetes", 
-                                    labels = c("Non-Diabetic ", "Diabetic "),
-                                    values =c("#F8766D", "#00BFC4"))
+scatterplot2 <- ggplot(data, aes(x = Age, y = Glucose, col = PredDiabetes)) +
+  geom_point() +
+  theme_classic() + 
+  ggtitle("Glucose vs. Age coloured by Predicted Diabetes Level")
 
 scatterplot2
 
 # Task 3
 
-intercept <- fit$coefficients[1]/(-fit$coefficients[3]) 
+intercept <- glm_fit$finalModel$coefficients[1]/(-glm_fit$finalModel$coefficients[3]) 
 
-slope <- fit$coefficients[2]/(-fit$coefficients[3])
+slope <- glm_fit$finalModel$coefficients[2]/(-glm_fit$finalModel$coefficients[3])
 
 scatterplot3 <- scatterplot2 + geom_abline( intercept = intercept, slope = slope)
 
@@ -57,44 +65,43 @@ scatterplot3
 
 # Task 4
 
-predict_0.2 <- as.factor(ifelse(predict(fit, data, type="response")>0.2,"neg","pos"))
+data$PredDiabetes2 <- as.factor(ifelse(predict(glm_fit,
+                                             type="prob") < 0.2,
+                                     "No","Yes")[,2])
 
-data$Predict_0.2 <- predict_0.2
+misclass(data$Diabetes, data$PredDiabetes2)
 
-misclass(data$Diabetes, data$Predict_0.2)
-
-scatterplot3 <- ggplot(data, aes(x = Age, y = Plasma, col = Predict_0.2)) +
+scatterplot3 <- ggplot(data, aes(x = Age, y = Glucose, col = PredDiabetes2)) +
   geom_point()
 
 scatterplot3
 
-predict_0.8 <- as.factor(ifelse(predict(fit, data, type="response")>0.8,"neg","pos"))
-data$Predict_0.8 <- predict_0.8
+data$PredDiabetes8 <- as.factor(ifelse(predict(glm_fit,
+                                             type="prob") < 0.8,
+                                     "No","Yes")[,2])
 
-misclass(data$Diabetes, data$Predict_0.8)
+misclass(data$Diabetes, data$PredDiabetes8)
 
-scatterplot4 <- ggplot(data, aes(x = Age, y = Plasma, col = Predict_0.8)) +
+scatterplot4 <- ggplot(data, aes(x = Age, y = Glucose, col = PredDiabetes8)) +
   geom_point()
 
 scatterplot4
 
 # Task 5
 
-data$z1 <- data$Plasma^4
-data$z2 <- data$Plasma^3 * data$Age
-data$z3 <- data$Plasma^2 * data$Age^2
-data$z4 <- data$Plasma * data$Age^3
+data$z1 <- data$Glucose^4
+data$z2 <- data$Glucose^3 * data$Age
+data$z3 <- data$Glucose^2 * data$Age^2
+data$z4 <- data$Glucose * data$Age^3
 data$z5 <- data$Age^4
 
-fit2 <- glm(Diabetes ~ Age + Plasma + z1 + z2 + z3 + z4 + z5, data = data, family = binomial)
-probabilities <- predict(object = fit2, newdata =  data, type = "response")
-predicted_values2 <- ifelse(probabilities > 0.5, "pos", "neg")
+glm_fit_new <- train(Diabetes ~ Age + Glucose + z1 + z2 +
+                       z3 + z4 + z5, data = data, method="glm")
+data$PredDiabetesNew <- predict(glm_fit_new) 
 
-data$Predict2 <- predicted_values2
+misclass(data$Diabetes, data$PredDiabetesNew)
 
-misclass(data$Diabetes, data$Predict2)
-
-scatterplot5 <- ggplot( data = data, aes( x = Age, y = Plasma, colour = Predict2)) +
+scatterplot5 <- ggplot( data = data, aes( x = Age, y = Glucose, colour = PredDiabetesNew)) +
   geom_point()
 
 scatterplot5
